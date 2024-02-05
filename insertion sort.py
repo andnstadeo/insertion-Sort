e2de91600868627ec3d5b48c5139b9465f82fa3f
Ann Denise Tadeo <150144454+andnstadeo@users.noreply.github.com>
2024-02-05 10:12:14 +0800

    insertion sort.py

--- insertion sort.py
+++ insertion sort.py
@@ -1,3 +1,7 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
 def perform_insertion_sort(input_list):
     length = len(input_list)
 
@@ -7,15 +11,22 @@ def perform_insertion_sort(input_list):
 
         while j >= 0 and key < input_list[j]:
             input_list[j + 1] = input_list[j]
-            j -= 1
+            j -= 1  
 
         input_list[j + 1] = key
 
-if __name__ == "__main__":
-    user_input = input("Enter a list of numbers separated by spaces: ")
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/sort', methods=['POST'])
+def sort():
+    user_input = request.form['numbers']
     unsorted_list = [int(x) for x in user_input.split()]
 
-    print("Unsorted list:", unsorted_list)
     perform_insertion_sort(unsorted_list)
 
-    print("Sorted list:", unsorted_list)
+    return render_template('result.html', unsorted_list=user_input, sorted_list=unsorted_list)
+
+if __name__ == "__main__":
+    app.run(debug=True)
